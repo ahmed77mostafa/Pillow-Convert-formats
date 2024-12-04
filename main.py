f613b86d325cbbeb0dbdb08243dedaa83b7182e3
ahmed77mostafa <ahmedmostafa.0522030@gmail.com>
2024-12-04 08:36:59 +0200

    Convert formats commit

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ img = Image.open("images/𝟵𝗔𝗗 __ صَاد.jpg")
 img.show()
 img_RGP = img.convert("RGB")
 img_RGP.show()
+img_resized = img.resize((800,600))
+img_resized.show()
 img.save("C:/Users/ahmed.mostafa/PycharmProjects/Pillow/S@D.jpg")
 
 print(f"image size: {img.size},",f"image format: {img.format},",f"image mode: {img.mode}")
